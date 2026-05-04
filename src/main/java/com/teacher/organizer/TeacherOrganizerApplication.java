0874935794e7a9b813d67d82b620777ce9300c63
nicolasholanda <nikolas.nhm14@gmail.com>
2026-05-04 09:06:56 -0300

    Initial commit

---
+++ src/main/java/com/teacher/organizer/TeacherOrganizerApplication.java
@@ -0,0 +1,12 @@
+package com.teacher.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherOrganizerApplication.class, args);
+    }
+}
